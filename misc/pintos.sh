f555260ec354be10296a108606e801be5f44a255
Ana-Maria Cusco <anamaria_cusco@yahoo.com>
2022-10-06 00:50:49 +0300

    Initial commit

---
+++ misc/pintos.sh
@@ -0,0 +1,2 @@
+export PATH=/home/ami/D/Facultate/An_4/PSO_An4_Sem1/pintos/src/utils:/home/ami/D/Facultate/An_4/PSO_An4_Sem1/pintos/bochs-2.6.8/bin:$PATH
+export BXSHARE=/home/ami/D/Facultate/An_4/PSO_An4_Sem1/pintos/bochs-2.6.8/share/bochs
