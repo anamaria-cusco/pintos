e6b17bdccd4a5cd54317a0f6520436a545f6f1cf
Ana-Maria Cusco <anamaria_cusco@yahoo.com>
2022-10-24 23:00:51 +0300

    Lab 2: Part 2.1

---
+++ tests/threads/my-test-create-threads.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "threads/thread.h"
+
+void create_threads(void);
+void th_func_to_exec(void *aux UNUSED);
+void print_all_threads_info();
+
+#define NUM_THREADS 5
+
+int total_thread_no;
+
+void th_func_to_exec(void *aux UNUSED)
+{
+    struct thread *current_th = thread_current();
+    print_thread_info(current_th);
+}
+
+/* Prints info about all threads in the system */
+void print_all_threads_info()
+{
+    intr_disable();
+    thread_foreach(print_thread_info, NULL);
+    intr_enable();
+}
+
+void my_test_create_threads()
+{
+    char name[16];
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        snprintf(name, sizeof name, "my_thread_%d", i);
+        thread_create(name, PRI_DEFAULT, print_all_threads_info, (void *)NULL);
+    }
+}
--- tests/threads/tests.h
+++ tests/threads/tests.h
@@ -1,9 +1,9 @@
 #ifndef TESTS_THREADS_TESTS_H
 #define TESTS_THREADS_TESTS_H
 
-void run_test (const char *);
+void run_test(const char *);
 
-typedef void test_func (void);
+typedef void test_func(void);
 
 extern test_func test_alarm_single;
 extern test_func test_alarm_multiple;
@@ -33,10 +33,10 @@ extern test_func test_mlfqs_nice_2;
 extern test_func test_mlfqs_nice_10;
 extern test_func test_mlfqs_block;
 extern test_func my_test;
+extern test_func my_test_create_threads;
 
-void msg (const char *, ...);
-void fail (const char *, ...);
-void pass (void);
+void msg(const char *, ...);
+void fail(const char *, ...);
+void pass(void);
 
 #endif /* tests/threads/tests.h */
-
